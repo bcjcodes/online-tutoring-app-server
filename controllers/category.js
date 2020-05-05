fae3eb61df4176687e4df23f6f5b5abbc7b58b7b
bcjcodes <blessingakpanemmanuel@gmail.com>
2020-05-05 08:44:05 +0100

    adding more files

--- controllers/category.js
+++ controllers/category.js
@@ -24,8 +24,52 @@ exports.createCategories = (req, res, next) => {
   })
 }
 
+exports.viewEachCategory = (req, res, next) => {
+  let id = req.params.id
+  Category.findOne({ _id: id })
+    .then(category => {
+      res.json(category)
+    })
+    .catch(err => console.log(err))
+}
+
 exports.viewCategories = (req, res, next) => {
   Category.find({}).then(category => {
     res.send(category)
+  })
+}
+
+exports.updateCategory = (req, res, next) => {
+  let id = req.params.id
+  let { name } = req.body
+
+  Category.findByIdAndUpdate(id, { name: name })
+    .then(category => {
+      if (category) {
+        return res.send('Updated Successfully')
+      } else {
+        res.status(404).send('category does not exist')
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+exports.deleteCategory = (req, res, next) => {
+  let categoryId = req.params.id
+
+  Category.findById(categoryId).then(category => {
+    if (!category) {
+      res.status(404).send('Category not found')
+    } else {
+      category.remove(err => {
+        if (!err) {
+          Subject.deleteMany({ category: categoryId }).then(() => {
+            res.send(category)
+          })
+        } else {
+          console.log(err)
+        }
+      })
+    }
   })
 }
--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
 
-const { createCategories, viewCategories } = require('../controllers/category')
+const {
+  createCategories,
+  viewCategories,
+  viewEachCategory,
+  deleteCategory,
+  updateCategory
+} = require('../controllers/category')
 
 router.post('/category', createCategories)
 router.get('/category', viewCategories)
+router.get('/category/:id', viewEachCategory)
+router.put('/category/:id', updateCategory)
+router.delete('/category/:id', deleteCategory)
 
 module.exports = router
